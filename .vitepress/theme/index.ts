feb4d92507a6ffea9aaac2f83d8426f460dc9541
star-nebula <3089068940@qq.com>
2026-06-30 09:39:49 +0800

    ``` feat(theme): 添加NoteExplorer组件并更新笔记页面结构
    - 注册NoteExplorer组件到主题中
    - 在笔记索引页面替换原有内容为NoteExplorer组件
    - 移除多余的部署相关文档页面
    - 更新网站部署目录的链接格式
    ```

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -7,6 +7,7 @@ import { h } from 'vue'
 import AppContainer from './components/AppContainer.vue'
 import DocFooter from './components/DocFooter.vue'
 import HomePage from './components/HomePage.vue'
+import NoteExplorer from './components/NoteExplorer.vue'
 import PageMeta from './components/PageMeta.vue'
 import Share from './components/Share.vue'
 
@@ -67,6 +68,7 @@ const ExtendedTheme: Theme = {
 
     app.component('HomePage', HomePage)
     app.component('DocFooter', DocFooter)
+    app.component('NoteExplorer', NoteExplorer)
     app.component('PageMeta', PageMeta)
     app.component('Share', Share)
     app.component('AppContainer', AppContainer)
--- zh-CN/笔记/index.md
+++ zh-CN/笔记/index.md
@@ -2,26 +2,10 @@
 
 欢迎来到笔记分区，这里记录了学习、生活和思考的方方面面。
 
+点击分类名称进入对应目录，展开可浏览目录下的所有文章。
+
+<NoteExplorer />
+
 ## 关于
 
 本知识库使用 [VitePress](https://vitepress.dev) 构建，使用 [Obsidian](https://obsidian.md) 管理笔记。
-
-## 如何使用
-
-### 安装依赖
-
-```shell
-pnpm install
-```
-
-### 运行开发服务器
-
-```shell
-pnpm docs:dev
-```
-
-### 构建静态页面
-
-```shell
-pnpm docs:build
-```
